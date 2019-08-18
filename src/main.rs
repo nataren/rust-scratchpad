73c9d0ea83e698c65c1ead4d862d9d53fe5a74a6
César López-Natarén <cesar@nataren.io>
2019-08-18 00:29:14 -0700

    Start using a logging package

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::{thread, time};
+use log::{info, warn};
 
 fn main() {
+    env_logger::init();
+    info!("starting up");
+
     let one_millis = time::Duration::from_millis(1);
 
     let pb = indicatif::ProgressBar::new(100);
@@ -10,4 +14,6 @@ fn main() {
         pb.inc(1);
     }
     pb.finish_with_message("done");
+
+    warn!("nothing implemented...");
 }
